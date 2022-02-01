139146063a3c1f471e5032d26fcae35acff9cf3d
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-02-01 16:01:25 +0000

    Initial commit

---
+++ Week2/Three/Main.java
@@ -0,0 +1,6 @@
+// This program prints Welcome to Java!
+class Main {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Intro to Java I at Mount Greylock!");
+    }
+}
---
+++ Week2/Welcome/Main.java
@@ -0,0 +1,8 @@
+// This program displays three different statements
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("This is Intro to Java I");
+        System.out.println("I enjoy the snow!");
+    }
+}
