02ffb7b7dbfb4b8cda65ddc75f9f325b2b57f0b6
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-02-07 19:39:55 +0000

    Updated 2/7

--- Week3/Calculate.java
+++ Week3/Calculate.java
@@ -10,5 +10,27 @@ class Calculate {
         // Addition
         int addition = first + second;
         System.out.println(first + " plus " + second + " equals " + addition);
+
+        // Subtraction
+        int subtraction = first - second;
+        System.out.println(first + " minus " + second + " equals " + subtraction);
+
+        // Multiplication
+        int multiplication = first * second;
+        System.out.println(first + " times " + second + " equals " + multiplication);
+
+        // Integer Division
+        int division = first / second;
+        System.out.println(first + " divided by " + second + " equals " + division);
+
+        // Division with a double
+        double firstDouble = 5.0;
+        double secondDouble = 2.0;
+        double divisionDouble = firstDouble / secondDouble;
+        System.out.println(firstDouble + " divided by " + secondDouble + " equals " + divisionDouble);
+
+        // Mod - this is like asking "What is the remainder?"
+        int mod = first % second;
+        System.out.println(first + " mod " + second + " equals " + mod);
     }
 }
