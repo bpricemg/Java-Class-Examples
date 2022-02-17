a5fb5af1d966276bf665814480a434dde6565ffc
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-02-17 17:00:45 +0000

    Updated 2/17

--- Week4/Calculator.java
+++ Week4/Calculator.java
@@ -19,21 +19,21 @@ class Calculator {
 
         // Do some calculations and print them out
         int add = x + y;
-        System.out.println("Added: " + add);
+        System.out.println("x plus y is " + add);
 
         int subtract = x - y;
-        System.out.println("Subtrcted: " + subtract);
+        System.out.println("x minus y is " + subtract);
 
         // Typecast x and y to a double for division
         double divide = (double)x / (double)y;
-        System.out.println("Divided: " + divide);
+        System.out.println("x divided by y is " + divide);
 
         int multiply = x * y;
-        System.out.println("Multiplied: " + multiply);
+        System.out.println("x times y is " + multiply);
 
         // Math.pow() will return a double so we typecast to an int
         int power = (int)Math.pow(x, y);
-        System.out.println("x to the y power: " + power);
+        System.out.println("x raised to the power of y is " + power);
         
     }
 }
