aa1366e5c34529009d02850f03d64145f2876b4f
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-03-08 18:24:50 +0000

    Updated 3/8

---
+++ Week6/AdditionQuiz.java
@@ -0,0 +1,31 @@
+/**
+ * Addition quiz for an elementary student
+ */
+
+import java.util.Scanner;
+
+class AdditionQuiz {
+    public static void main(String[] args) {
+        // Create two random integers between 0 and 9 (inclusive)
+        int random1 = (int) (Math.random() * 10);
+        int random2 = (int) (Math.random() * 10);
+
+        // Ask the user what the sum of those two ints are
+        System.out.print("What is " + random1 + " plus " + random2 + "? ");
+
+        // Get the user's input with a Scanner
+        Scanner input = new Scanner(System.in);
+        int answer = input.nextInt();
+
+        // If they are correct, print "You are correct"
+        if (answer == random1 + random2) {
+            System.out.println("You are correct!");
+        }
+
+        // Else, if they are incorrect, print "Sorry, you are not correct. 
+        // The correct answer is..."
+        else {
+            System.out.println("Sorry, you are not correct. The correct answer is " + (random1                                 + random2) + ".");
+        }
+    }
+}
